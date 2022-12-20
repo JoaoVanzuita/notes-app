acd3869ac09eb53d1f70896d64fc4ebc2050df39
Joao Vanzuita <joaog17vanzuita@gmail..com>
2022-12-20 20:17:45 -0300

    fix cors error

--- server/src/app.ts
+++ server/src/app.ts
@@ -11,7 +11,7 @@ app.use(cookieParser())
 app.use(express.json())
 
 app.use(cors({
-  origin: ['http://127.0.0.1:5173', 'http://localhost:5173'],
+  origin: ['http://127.0.0.1:5173', 'http://localhost:5173', 'https://notes-app-t8oi.onrender.com/'],
   credentials: true,
   methods: ['POST', 'GET', 'PUT', 'DELETE', 'OPTIONS'],
   exposedHeaders: ['*', 'Authorization'],
