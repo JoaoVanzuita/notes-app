9b673457fb2e98d2be7e897cbf045d07ba338786
Joao Vanzuita <joaog17vanzuita@gmail..com>
2022-12-20 20:10:22 -0300

    (trying to) fix api not found error on client

--- client/src/shared/services/api/axios-config/index.ts
+++ client/src/shared/services/api/axios-config/index.ts
@@ -3,7 +3,7 @@ import { Environment } from '../../../environment'
 import { errorInterceptor } from '../errors/errorInterceptor'
 
 const Api = axios.create({
-  baseURL: Environment.BASE_URL,
+  baseURL: Environment.BASE_URL ?? 'https://notes-app-server.onrender.com',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json'
--- client/src/shared/services/api/errors/errorInterceptor.ts
+++ client/src/shared/services/api/errors/errorInterceptor.ts
@@ -1,8 +1,10 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import { AxiosError } from 'axios'
 import { ResponseError } from '.'
 
 export const errorInterceptor = (error: AxiosError<{message:string, status:number}>) => {
 
-  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-  throw new ResponseError(error.response!.data.message, error.response!.data.status)
+  if(error.response!.data.message && error.response!.data.status){
+    throw new ResponseError(error.response!.data.message, error.response!.data.status)
+  }
 }
