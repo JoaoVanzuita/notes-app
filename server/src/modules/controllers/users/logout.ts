2b0a65081950f165dad18ebcb8cad797560f54dd
Joao Vanzuita <joaog17vanzuita@gmail..com>
2022-12-20 23:12:21 -0300

    update cookie options on Login and Logout

--- server/src/modules/controllers/users/logout.ts
+++ server/src/modules/controllers/users/logout.ts
@@ -3,8 +3,6 @@ import { Request, Response } from 'express'
 export const logout = async (req: Request, res: Response) => {
 
   return res.clearCookie('accessToken', {
-    domain: 'https://notes-app-t8oi.onrender.com/',
-    path: '/',
     sameSite: 'none',
     secure: true,
     httpOnly: true,
