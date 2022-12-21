63a0014fb0731d7716d4171f5a53439c4fae5dd4
Joao Vanzuita <joaog17vanzuita@gmail..com>
2022-12-20 22:28:08 -0300

    trying to fix set cookie on production

--- client/src/shared/environment/index.ts
+++ client/src/shared/environment/index.ts
@@ -1,4 +1,4 @@
 export const Environment = {
-  BASE_URL: 'http://localhost:8080' ?? 'https://notes-app-server.onrender.com/',
+  BASE_URL: 'https://notes-app-server.onrender.com/',
   SERVER_ERROR: 'Ocorreu um erro desconhecido ao consultar o servidor.'
 }
